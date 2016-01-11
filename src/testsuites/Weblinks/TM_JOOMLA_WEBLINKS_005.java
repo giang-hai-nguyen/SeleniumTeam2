a74d4449dec4439bf86b38704907816344350659
thuhang91 <thuhang09cntt2@gmail.com>
2016-01-11 22:34:30 +0700

    add TM005 of Weblinks

---
+++ src/testsuites/Weblinks/TM_JOOMLA_WEBLINKS_005.java
@@ -0,0 +1,62 @@
+package Weblinks;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import ac_common.*;
+import ac_pages.*;
+import config.*;
+
+public class TM_JOOMLA_WEBLINKS_005 extends ac_WeblinksPage{
+	
+	@BeforeClass
+	@Parameters({ "browser" })
+	public void Setup(@Optional("firefox") String browser) {
+		driver = BrowserExecution.navigateJoomla(browser);
+		LoginPage = new ac_LoginPage(driver);
+		LoginPage.Login(Config.default_username, Config.default_password);
+	}
+
+	@Test(description = "Verify user can create a copied version of an existed weblink")
+	public void TC_JOOMLA_WEBLINKS_016()
+	{
+		WeblinksPage = new ac_WeblinksPage(driver);
+		WeblinksPage.navigatemenu(driver, "Components", "Weblinks", null);
+		WeblinksPage.clickToolbarButton(driver, "new");
+		WeblinksPage.fillInfoWeblinks (weblinks_title1, weblinks_url1, null, null);
+		WeblinksPage.clickToolbarButton(driver, "save");
+		verifyTrue(WeblinksPage.doesTextPresent(driver, message_create));
+		verifyTrue(WeblinksPage.doesitemExist(driver, weblinks_title1));
+		
+		WeblinksPage.selectCheckboxItemWithoutDiv(driver, weblinks_title1);
+		WeblinksPage.clickToolbarButton(driver, "edit");
+		WeblinksPage.fillInfoWeblinks (weblinks_title2, weblinks_url2, null, null);
+		WeblinksPage.clickToolbarButton(driver, "save-copy");
+		verifyTrue(WeblinksPage.doesTextPresent(driver, message_create));
+		WeblinksPage.clickToolbarButton(driver, "cancel");
+		verifyTrue(WeblinksPage.doesitemExist(driver, weblinks_title2 + " (2)"));
+		
+	}	
+	
+	@AfterClass
+	public void teardown(){
+		AdminPage = new ac_AdministratorPage(driver);
+		AdminPage.Logout();		
+		BrowserExecution.closeJoomla();
+	}
+	
+	private WebDriver driver;
+	private ac_LoginPage LoginPage;	
+	private ac_AdministratorPage AdminPage;
+	private ac_WeblinksPage WeblinksPage;
+	
+	private String weblinks_title1 = randUniqueString("Test Weblink");
+	private String weblinks_url1 = "http://www.joomla.org";
+	private String message_create = "Web link successfully saved";
+	private String weblinks_title2 = randUniqueString("Test Weblink");
+	private String weblinks_url2 = "http://www.google.com";
+}
