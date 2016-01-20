673be9f942d2185299afae350867afa4d2d63b85
ZicZac <nbaotuan@gmail.com>
2016-01-20 18:21:19 +0700

    commit branch 1

--- src/interfaces/in_pages/in_ArticlesPage.java
+++ src/interfaces/in_pages/in_ArticlesPage.java
@@ -1,6 +1,5 @@
 package in_pages;
 
-import org.openqa.selenium.By;
 
 public class in_ArticlesPage {
 	
@@ -40,14 +39,6 @@ public class in_ArticlesPage {
 	
 	
 	public static final String arttext_frame_textbox="//iframe[@id='jform_articletext_ifr']";
-	public static final String frame_textbox="//body[@id='tinymce']";
-	public static final String header_ID = "//form[@id='adminForm']//table/thead/tr//a[contains(text(),'ID')]";
-	public static final String table_admin = "//form[@id='adminForm']//table/tbody";
-	public static By dropdownlist_paginate = By.xpath("//select[@id='list_limit']");
-	public static final String clear_btn = "//form[@id='adminForm']//button[contains(.,'Clear')]";
-	public static final String header_ordering ="//table[@id='articleList']//tr/th/a/span";
-	public static final String order_arrow_up = "//table[@id='articleList']//tr/th/a/span[@class='icon-arrow-up-3']";
-	public static final String order_arrow_down = "//table[@id='articleList']//tr/th/a/span[@class='icon-arrow-down-3']";
-	
-	
+	public static final String frame_textbox="//body[@id='tinymce']";	
+		
 }
